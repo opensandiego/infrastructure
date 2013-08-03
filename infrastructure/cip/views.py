074a6d4ea8088858b1bd8e773810c04d777199ff
Mila Frerichs <mila.frerichs@gmail.com>
2013-08-03 08:14:59 +0200

    refactored filtering to use ModelManager

--- infrastructure/cip/views.py
+++ infrastructure/cip/views.py
@@ -1,31 +1,43 @@
 # Create your views here.
 import datetime
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.db.models import Q
-from infrastructure.cip.models import Project, DepartmentNeed
+from infrastructure.cip.models import *
+from django import forms
+from django_select2 import *
+
 def index(request):
     """docstring for projects"""
     projects = Project.objects.all()
     department_needs = DepartmentNeed.objects.all()
     return render_to_response('index.haml', {'projects': projects, 'department_needs': department_needs})
 
-def projects(request,filter ='', value= ''):
+def projects(request,filter ='', value= '',show= 'current'):
     """docstring for projects"""
+    if show == "all":
+        projects = Project.objects.all
+    else:
+        projects = Project.objects.current()
     if filter != "":
         if filter == "phase":
-            filter_q = Q(SP_PROJECT_PHASE__icontains=value)
-            projects = Project.objects.filter(filter_q & current_project_q()).order_by('SP_CONSTR_FINISH_DT')
+            if value == "planning":
+                projects = Project.objects.future()
+            projects = projects.by_phase(value)
         elif filter == "asset_group":
-            filter_q = Q(SP_ASSET_TYPE_GROUP=value)
-            projects = Project.objects.filter(filter_q & current_project_q()).order_by('SP_CONSTR_FINISH_DT')
+            project = projects.by_asset_group(value)
+    if request.POST:
+        form = InitialValueForm(request.POST)
+        form.is_valid()
     else:
-        projects = Project.objects.filter(current_project_q()).order_by('SP_CONSTR_FINISH_DT')
-    return render_to_response('projects.haml', {'projects': projects})
+        form = InitialValueForm()
+    return render(request, 'projects.haml', {'projects': projects, 'form': form })
 
-def current_project_q():
-    return Q(SP_CONSTR_FINISH_DT__gt=datetime.date(2013, 8, 1), SP_AWARD_START_DT__lt=datetime.date(2013, 8, 1))
 def show_project(request, p_id):
     """docstring for show_project"""
     project = Project.objects.get(id= p_id)
     
     return render_to_response('project.haml', {'project': project})
+
+class InitialValueForm(forms.Form):
+    departments = Select2ChoiceField(initial=2,
+        choices=PROJECT_PHASES)
