c5fd9d798fecda7dd9149fed4564492c5a6fce9f
Mila Frerichs <mila.frerichs@gmail.com>
2013-08-06 22:15:58 +0200

    detail view updated, link to detail view updated
    root view now shows projects view by default

--- infrastructure/urls.py
+++ infrastructure/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from infrastructure.cip.views import ProjectList, ProjectDetailView
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 admin.autodiscover()
 
 urlpatterns = patterns('infrastructure.cip.views',
     # Examples:
     # url(r'^$', 'infrastructure.views.home', name='home'),
     # url(r'^infrastructure/', include('infrastructure.foo.urls')),
-    url(r'^$', 'index'),
+    url(r'^$',ProjectList.as_view()), 
     url(r'^projects$',ProjectList.as_view(), name='projects_list'),
     url(r'^projects/(?P<filter>\w+([\w_])*)/(?P<value>\w+([\/\w \.-?%?&]*))$', ProjectList.as_view(), name='projects_filter_list'),
     url(r'^projects/(?P<filter>\w+)/(?P<value>\w+(\s+\w+)*)/(?P<show>(all|current))$', ProjectList.as_view(), name='projects_list'),
